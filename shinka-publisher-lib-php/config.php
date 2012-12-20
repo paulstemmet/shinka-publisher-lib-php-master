243083709d77878a1b65ad69e7273e75b788939f
Paul Stemmet <paul.stemmet@gmail.com>
2012-12-20 12:23:26 +0200

    Commit first

---
+++ shinka-publisher-lib-php/config.php
@@ -0,0 +1,14 @@
+<?php 
+define('TIMEOUT',10);
+define('IS_RESIZE_IMAGES',FALSE);
+define('REFERER','YOUR_APP_MXIT_ID');
+define('TESTUSER','PUT_YOUR_MXITUSERID_HERE_TO_DEBUG');
+
+define('API_SERVER','http://ox-d.shinka.sh/ma/1.0/arj');
+				
+define('AdUnitID_320',"31XXXX");
+define('AdUnitID_216',"31XXXX");
+define('AdUnitID_168',"31XXXX");
+define('AdUnitID_120',"31XXXX");		
+
+?>
